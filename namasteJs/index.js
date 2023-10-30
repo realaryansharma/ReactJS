d1c07c2e103b6c80577ff5ac03039a54a192dc04
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2023-10-30 11:10:09 +0530

    late push

--- namasteJs/index.js
+++ namasteJs/index.js
@@ -1,9 +1,27 @@
+//Closure Example Ep 10
+
+// function x() {
+//     let a = 10;
+//     function y() {
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+//Ep 11
+
 function x() {
+
     let a = 10;
-    function y() {
+
+    setTimeout( function() {
         console.log(a);
-    }
-    y();
+        
+    }, 3000);
+
+    console.log("Ayoo..!");
+
 }
 
 x();
